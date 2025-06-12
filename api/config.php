5f45de56dd0d94e440278d108707bbd4e1b66873
joelobros23 <laberintoj8@gmail.com>
2025-06-13 05:28:13 +0800

    feat: Set up the database connection (api/config.php).

--- api/config.php
+++ api/config.php
@@ -1,3 +1,15 @@
 <?php
-// PHP code will be generated here.
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "chromaconnect";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
 ?>
